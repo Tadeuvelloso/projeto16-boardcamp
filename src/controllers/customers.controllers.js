09803347d08622f5313c4784e551fef2bde9fe0d
Tadeuvelloso <tadeuvellososilva@gmail.com>
2022-12-12 13:40:23 -0300

    feat: put customers controller

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -10,3 +10,15 @@ export async function postNewCustomer(req, res) {
         return res.staus(500).send(error.message);
     }
 }
+
+export async function putCustomer(req, res) {
+    const {name, phone, cpf, birthday} = res.locals.customer;
+    const id = req.params;
+
+    try {
+        await connectionDB.query(`UPDATE customers (name=$1, phone=$2, cpf=$3, birthday=$4) WHERE id=$5;`, [name, phone, cpf, birthday, id]);
+        res.sendStatus(200);
+    } catch (error) {
+        return res.staus(500).send(error.message);
+    }
+}
