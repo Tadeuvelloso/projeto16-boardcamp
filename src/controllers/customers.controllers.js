67e151e7def3a1fded1466fc3e1f6853a00265f5
Tadeuvelloso <tadeuvellososilva@gmail.com>
2022-12-12 13:52:55 -0300

    feat: get all customer controller

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -1,7 +1,7 @@
 import { connectionDB } from "../database/db.js";
 
 export async function postNewCustomer(req, res) {
-    const {name, phone, cpf, birthday} = res.locals.customer;
+    const { name, phone, cpf, birthday } = res.locals.customer;
 
     try {
         await connectionDB.query(`INSER INTO customers (name, phone, cpf, birthday) VALUES (1$, 2$, 3$, 4$) ;`, [name, phone, cpf, birthday]);
@@ -12,13 +12,23 @@ export async function postNewCustomer(req, res) {
 }
 
 export async function putCustomer(req, res) {
-    const {name, phone, cpf, birthday} = res.locals.customer;
-    const {id} = res.locals.id;
+    const { name, phone, cpf, birthday } = res.locals.customer;
+    const { id } = res.locals.id;
 
     try {
         await connectionDB.query(`UPDATE customers (name=$1, phone=$2, cpf=$3, birthday=$4) WHERE id=$5;`, [name, phone, cpf, birthday, id]);
         res.sendStatus(200);
     } catch (error) {
         return res.staus(500).send(error.message);
+    }
+}
+
+export async function getAllCustomers(req, res) {
+
+    try {
+        const allCustomers = await connectionDB.query("SELECT * FROM customers;");
+        res.send(allCustomers.rows);
+    } catch (error) {
+        return res.status(500).send(error.message);
     }
 }
